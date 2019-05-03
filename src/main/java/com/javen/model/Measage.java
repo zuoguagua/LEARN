88f83f27cbf1afb76794c9341fbe08f7523bab6e
zuoqiang <zuoqiang@zuoqiang01.home.langchao.com>
2019-05-03 17:38:03 +0800

    Project Create Success.

---
+++ src/main/java/com/javen/model/Measage.java
@@ -0,0 +1,25 @@
+package com.javen.model;
+
+public class Measage {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	private String info;
+}
